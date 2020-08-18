409baf05431946bbc42eeb15be304385278eb9fb
paulayo93 <ayopaulofficial@gmail.com>
2020-08-18 17:38:36 +0100

    1 file added

---
+++ src/app/connect/connect.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+// firebase import
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: []
+})
+export class ConnectModule { }
