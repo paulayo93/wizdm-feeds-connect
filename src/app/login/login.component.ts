7d3c0119dcb7b1d7e1b7aae4002a2e626c6ce82f
paulayo93 <ayopaulofficial@gmail.com>
2020-08-20 14:15:03 +0100

    3 files modified, 4 added

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,38 +1,29 @@
 import { Component, OnInit } from "@angular/core";
 import { FormGroup, FormControl, Validators } from "@angular/forms";
 import { Router, ActivatedRoute, ParamMap } from "@angular/router";
+import { $loginAnimations } from "./login-animations";
+import { $authProviders } from "./providers";
+import { AuthService } from "@wizdm/connect/auth";
 
-
-type pageTypes = 'register' | 'signIn' | 'forgotPassword' | 'resetPassword' | 'changePassword' | 'changeEmail' | 'delete';
+type pageTypes =
+  | "register"
+  | "signIn"
+  | "forgotPassword"
+  | "resetPassword"
+  | "changePassword"
+  | "changeEmail"
+  | "delete";
 
 let $msgs = {
-  register: {// Register new user page
-    title: 'Register',
-    caption: 'Register with my email' 
-  }, 
-  signIn: { // Regular sign-in page
-    title: 'Sign-in',
-    caption: 'Sign-in with my email' 
+  register: {
+    // Register new user page
+    title: "Register",
+    caption: "Register with my email"
   },
-  forgotPassword: {// Ask for password reset page
-    title: 'Reset password',
-    caption: 'Reset the password' 
-  },
-  resetPassword: {// Reset to a new password page (2nd step after forgotPassword)
-    title: 'New password',
-    caption: 'Change the password' 
-  },
-  changeEmail: {// Change the email 
-    title: 'Change email',
-    caption: 'Update the email' 
-  },
-  changePassword: {// Change the password (while authenticated)
-    title: 'Change password',
-    caption: 'Update the password' 
-  },
-  delete: {// Delete the user account
-    title: 'Delete account',
-    caption: 'delete the account' 
+  signIn: {
+    // Regular sign-in page
+    title: "Sign-in",
+    caption: "Sign-in with my email"
   }
 };
 
@@ -46,7 +37,116 @@ export class LoginComponent implements OnInit {
   private name: FormControl;
   private email: FormControl;
   private password: FormControl;
-  constructor() {}
 
-  ngOnInit() {}
+  readonly msgs = $msgs;
+  public page: pageTypes;
+  private code: string;
+
+  private providers = $authProviders;
+  private hide = true;
+  public error = null;
+  public progress = false;
+
+  constructor(private route: ActivatedRoute, private auth: AuthService,private router: Router) {
+    this.name = new FormControl(null, Validators.required);
+    this.email = new FormControl(null, [Validators.required, Validators.email]);
+    this.password = new FormControl(null, Validators.required);
+
+    this.form = new FormGroup({});
+
+    this.switchPage("signIn");
+  }
+
+  private switchPage(page: pageTypes) {
+    // Removes all the controls from the form group
+    Object.keys(this.form.controls).forEach(control => {
+      this.form.removeControl(control);
+    });
+
+    // Add the relevant controls to the form according to selected page
+    switch ((this.page = page)) {
+      case "register":
+        this.form.addControl("name", this.name);
+        this.form.addControl("email", this.email);
+        this.form.addControl("password", this.password);
+        break;
+
+      default:
+      case "signIn":
+        this.form.addControl("email", this.email);
+        this.form.addControl("password", this.password);
+        break;
+    }
+  }
+
+  private signInWith(provider: string) {
+    this.progress = true;
+
+    // Signing-in with a provider
+    this.auth
+      .signInWith(provider)
+      .then(() => this.reportSuccess("Signed in using " + provider))
+      .catch(error => {
+        // Keep the rror code on failure
+        this.showError(error.code);
+      });
+  }
+
+  private signIn(email: string, password: string) {
+    this.progress = true;
+
+    // Sign-in using email/password
+    this.auth
+      .signIn(email, password)
+      .then(() => this.reportSuccess("Signed in as " + email))
+      .catch(error => {
+        // Keep the rror code on failure
+        this.showError(error.code);
+      });
+  }
+
+  private showError(error: string) {
+    this.error = error;
+    this.progress = false;
+    setTimeout(() => (this.error = null), 5000);
+  }
+
+  private reportSuccess(message: string, jumpTo?: string) {
+    this.progress = false;
+
+    console.log(message);
+
+    if (jumpTo) {
+      this.router.navigate(["."], {
+        relativeTo: this.route,
+        queryParams: {
+          mode: jumpTo
+        }
+      });
+    }
+  }
+
+  ngOnInit() {
+     this.route.queryParamMap.subscribe( (params: ParamMap) => {
+
+      let mode  = params.get('mode') || 'signIn';
+      this.code = params.get('oobCode');
+
+      console.log('login mode: ', mode);
+
+      switch(mode) {
+
+        case 'signOut':
+        this.signOut();
+        break;
+
+        case 'verifyEmail':
+        this.verifyEmail( this.code );
+        break;
+
+        default:
+        this.switchPage(mode as pageTypes);
+      }
+    });
+  }
 }
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {AuthModule} from '@wizdm/connect/auth'
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    AuthModule
   ],
   declarations: []
 })
