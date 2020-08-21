ee1d3b7fbd1dc087ed86ddc672899df05acf4291
paulayo93 <ayopaulofficial@gmail.com>
2020-08-21 03:59:06 +0100

    2 files modified, 1 added, 1 deleted

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,131 +3,86 @@ import { FormGroup, FormControl, Validators } from "@angular/forms";
 import { Router, ActivatedRoute, ParamMap } from "@angular/router";
 import { $loginAnimations } from "./login-animations";
 import { $authProviders } from "./providers";
-import { AuthService } from "@wizdm/connect/auth";
+import { AuthService, User, UserExtension } from "@wizdm/connect/auth";
 
-type pageTypes =
-  | "register"
-  | "signIn"
-  | "forgotPassword"
-  | "resetPassword"
-  | "changePassword"
-  | "changeEmail"
-  | "delete";
+type pageTypes = 'register' | 'signIn' | 'forgotPassword' | 'resetPassword' | 'changePassword' | 'changeEmail' | 'delete';
 
-// let $msgs = {
-//   register: {
-//     // Register new user page
-//     title: "Register",
-//     caption: "Register with my email"
-//   },
-//   signIn: {
-//     // Regular sign-in page
-//     title: "Sign-in",
-//     caption: "Sign-in with my email"
-//   }
-// };
+let $msgs = {
+  register: {// Register new user page
+    title: 'Register',
+    caption: 'Register with my email' 
+  }, 
+  signIn: { // Regular sign-in page
+    title: 'Sign-in',
+    caption: 'Sign-in with my email' 
+  },
+  forgotPassword: {// Ask for password reset page
+    title: 'Reset password',
+    caption: 'Reset the password' 
+  },
+  resetPassword: {// Reset to a new password page (2nd step after forgotPassword)
+    title: 'New password',
+    caption: 'Change the password' 
+  },
+  changeEmail: {// Change the email 
+    title: 'Change email',
+    caption: 'Update the email' 
+  },
+  changePassword: {// Change the password (while authenticated)
+    title: 'Change password',
+    caption: 'Update the password' 
+  },
+  delete: {// Delete the user account
+    title: 'Delete account',
+    caption: 'delete the account' 
+  }
+};
 
 @Component({
-  selector: "app-login",
-  templateUrl: "./login.component.html",
-  styleUrls: ["./login.component.css"]
+  selector : 'wm-login',
+  templateUrl : './login.component.html',
+  styleUrls : ['./login.component.scss'],
+  animations: $loginAnimations
 })
-export class LoginComponent implements OnInit {
-  private form: FormGroup;
-  private name: FormControl;
-  private email: FormControl;
-  private password: FormControl;
+export class LoginComponent implements OnInit, UserExtension {
 
-  // readonly msgs = $msgs;
+  readonly msgs = $msgs;
   public page: pageTypes;
   private code: string;
 
+  readonly form: FormGroup;
+  private name: FormControl;
+  private email: FormControl;
+  private password: FormControl;
+  private newEmail: FormControl;
+  private newPassword: FormControl;
+  
   private providers = $authProviders;
   private hide = true;
   public error = null;
   public progress = false;
+  
+  constructor(private auth: AuthService,
+              private route : ActivatedRoute,
+              private router: Router) {
 
-  constructor(private route: ActivatedRoute, private auth: AuthService,private router: Router) {
     this.name = new FormControl(null, Validators.required);
     this.email = new FormControl(null, [Validators.required, Validators.email]);
     this.password = new FormControl(null, Validators.required);
+    this.newEmail = new FormControl(null, [Validators.required, Validators.email]);
+    this.newPassword = new FormControl(null, Validators.required);
 
     this.form = new FormGroup({});
 
-    this.switchPage("signIn");
-  }
-
-  private switchPage(page: pageTypes) {
-    // Removes all the controls from the form group
-    Object.keys(this.form.controls).forEach(control => {
-      this.form.removeControl(control);
-    });
-
-    // Add the relevant controls to the form according to selected page
-    switch ((this.page = page)) {
-      case "register":
-        this.form.addControl("name", this.name);
-        this.form.addControl("email", this.email);
-        this.form.addControl("password", this.password);
-        break;
-
-      default:
-      case "signIn":
-        this.form.addControl("email", this.email);
-        this.form.addControl("password", this.password);
-        break;
-    }
-  }
-
-  private signInWith(provider: string) {
-    this.progress = true;
-
-    // Signing-in with a provider
-    this.auth
-      .signInWith(provider)
-      .then(() => this.reportSuccess("Signed in using " + provider))
-      .catch(error => {
-        // Keep the rror code on failure
-        this.showError(error.code);
-      });
-  }
-
-  private signIn(email: string, password: string) {
-    this.progress = true;
-
-    // Sign-in using email/password
-    this.auth
-      .signIn(email, password)
-      .then(() => this.reportSuccess("Signed in as " + email))
-      .catch(error => {
-        // Keep the rror code on failure
-        this.showError(error.code);
-      });
-  }
-
-  private showError(error: string) {
-    this.error = error;
-    this.progress = false;
-    setTimeout(() => (this.error = null), 5000);
-  }
-
-  private reportSuccess(message: string, jumpTo?: string) {
-    this.progress = false;
-
-    console.log(message);
-
-    if (jumpTo) {
-      this.router.navigate(["."], {
-        relativeTo: this.route,
-        queryParams: {
-          mode: jumpTo
-        }
-      });
-    }
+    this.switchPage('signIn');
   }
 
   ngOnInit() {
-     this.route.queryParamMap.subscribe( (params: ParamMap) => {
+
+    this.auth.extendUser(this);
+
+    // Discrimnate among the login option using the queryParameters
+    this.route.queryParamMap.subscribe( (params: ParamMap) => {
 
       let mode  = params.get('mode') || 'signIn';
       this.code = params.get('oobCode');
@@ -140,23 +95,150 @@ export class LoginComponent implements OnInit {
         this.signOut();
         break;
 
+        case 'verifyEmail':
+        this.verifyEmail( this.code );
+        break;
+
         default:
         this.switchPage(mode as pageTypes);
       }
     });
   }
-    private signOut() {
 
-    this.progress = true;
+  private switchPage(page: pageTypes) {
 
-    this.auth.signOut()
-      .then( () => {
-        this.reportSuccess('Signed out', 'signIn');
-      })
+    // Removes all the controls from the form group
+    Object.keys(this.form.controls).forEach( control => {
+      this.form.removeControl(control);
+    });
+    
+    // Add the relevant controls to the form according to selected page
+    switch(this.page = page) {
+
+      case 'register':
+      this.form.addControl('name', this.name);
+      this.form.addControl('email', this.email);
+      this.form.addControl('password', this.password);
+      break;
+
+      default:
+      case 'signIn':
+      this.form.addControl('email', this.email);
+      this.form.addControl('password', this.password);      
+      break;
+
+      case 'forgotPassword':
+      this.form.addControl('email', this.email);
+      break;
+
+      case 'resetPassword':
+      this.form.addControl('newPassword', this.newPassword);
+      break;
+
+      case 'changePassword':
+      this.form.addControl('password', this.password);
+      this.form.addControl('newPassword', this.newPassword);
+      break;
+
+      case 'changeEmail':
+      this.form.addControl('password', this.password);
+      this.form.addControl('newEmail', this.newEmail);
+      break;
+
+      case 'delete':
+      this.form.addControl('password', this.password);      
+      break;
+    }
+  }
+
+  private showError(error: string) {
+
+    this.error = error;
+    this.progress = false;
+    setTimeout(() => this.error = null, 5000);
+  }
+
+  private reportSuccess(message: string, jumpTo?: string) {
+    
+    this.progress = false;
+    
+    console.log(message);
+
+    if(jumpTo) {
+      
+      this.router.navigate(['.'], { 
+        relativeTo: this.route,
+        queryParams: {
+          mode: jumpTo
+        } 
+      });
+    }
+  }
+
+  public loginAction() {
+    
+    switch(this.page) {
+
+      default:
+      case 'signIn':
+      this.signIn( this.email.value, 
+                   this.password.value );
+      break;
+
+      case 'register':
+      this.registerNew( this.email.value, 
+                        this.password.value, 
+                        this.name.value );
+      break;
+
+      case 'forgotPassword':
+      this.forgotPassword( this.email.value );
+      break;
+
+      case 'resetPassword':
+      this.resetPassword(this.code, this.newPassword.value );
+      break;
+
+      case 'changePassword':
+      this.updatePassword( this.password.value,
+                           this.newPassword.value );
+      break;
+
+      case 'changeEmail':
+      this.updateEmail( this.password.value,
+                        this.newEmail.value );
+      break;
+
+      case 'delete':
+      this.deleteAccount( this.password.value );
+      break;
+    }
+  }
+
+  private signInWith(provider: string) { 
+
+    this.progress = true;;
+
+    // Signing-in with a provider    
+    this.auth.signInWith( provider )
+      .then( () => this.reportSuccess('Signed in using ' + provider) )
       .catch( error => {
         // Keep the rror code on failure
         this.showError(error.code);
       })
+  }
+
+  private signIn(email: string, password: string) {
+    
+    this.progress = true;
+
+    // Sign-in using email/password
+    this.auth.signIn(email, password)
+      .then( () => this.reportSuccess('Signed in as ' + email) )
+      .catch( error => {
+      // Keep the rror code on failure
+      this.showError(error.code);
+    });
   }
 
   private registerNew(email: string, password: string,name: string) {
@@ -171,4 +253,31 @@ export class LoginComponent implements OnInit {
         this.showError(error.code);
       });
   }
-}
+
+  public onUserCreate(user: User): Promise<boolean> {
+    console.log('Extending new user: ', user.uid);
+    return Promise.resolve(true);
+  }
+
+
+
+
+  public onUserDelete(user: User): Promise<boolean> {
+    console.log('Wiping user: ', user.uid);
+    return Promise.resolve(true);
+  }
+
+  private signOut() {
+
+    this.progress = true;
+
+    this.auth.signOut()
+      .then( () => {
+        this.reportSuccess('Signed out', 'signIn');
+      })
+      .catch( error => {
+        // Keep the rror code on failure
+        this.showError(error.code);
+      })
+  }
+}  
