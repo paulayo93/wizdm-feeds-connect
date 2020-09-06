3f2174bf531dc0802d88dc3b792c1583c3c85ef4
paulayo93 <ayopaulofficial@gmail.com>
2020-09-06 08:57:56 +0100

    1 file modified

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,7 +4,7 @@ import { Router, ActivatedRoute, ParamMap } from "@angular/router";
 import { $loginAnimations } from "./login-animations";
 import { $authProviders } from "./providers";
 import { AuthService, User } from "@wizdm/connect/auth";
-import {UserProfile} from '../user/user-profile.service';
+// import {UserProfile} from '../user/user-profile.service';
 
 type pageTypes = 'register' | 'signIn' | 'forgotPassword' | 'resetPassword' | 'changePassword' | 'changeEmail' | 'delete';
 
@@ -65,7 +65,7 @@ export class LoginComponent implements OnInit{
   public progress = false;
   
   constructor(private auth: AuthService,
-              private user: UserProfile,
+             /* private user: UserProfile,*/
               private route : ActivatedRoute,
               private router: Router) {
 
